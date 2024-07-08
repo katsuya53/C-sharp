5bc8df5c9d6bb159288a5002635fbdec432637a7
katsuya53 <redstar53hk@gmail.com>
2024-07-08 22:44:41 +0900

    Create 配列ー42kg以上の値だけを表示.cs

---
+++ 配列ー42kg以上の値だけを表示.cs
@@ -0,0 +1,11 @@
+float[] weights = { 41.2f, 42.5f, 44.9f, 43.2f, 
+                    45.1f, 43.2f, 42.7f };
+
+// 43kg以下の値だけを表示する
+for (int i = 0; i < weights.Length; i++)
+{
+    if (weights[i] <= 42)
+    {
+         Console.WriteLine(weights[i]);
+    }
+}
